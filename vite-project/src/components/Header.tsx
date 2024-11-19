8c66ad645941f1d5c29d5de69f80966d953a525a
Lay Cipriano <webdevlay@gmail.com>
2024-11-19 17:41:48 -0300

    header completed

---
+++ vite-project/src/components/Header.tsx
@@ -0,0 +1,40 @@
+import '../assets/Header.css'
+
+export default function Header() {
+    return (
+        <>
+            <header className='header'>
+                <div className='back-img'>
+                    <div className="info">
+
+                        <h1>Laysa Cipriano</h1>
+
+                        <div className="skills">
+                            <ul>
+                                <li>Desenvolvimento Web</li>
+                                <li>Front End</li>
+                                <li>Back End</li>
+                                <li>API's</li>
+                                <li>Python</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
+
+                        <div className="social">
+                            <div className="links">
+                                <a href="http://www.linkedin.com/in/lay-cipriano" target='_blank' className='item-link'><i className="fa-brands fa-linkedin"></i></a>
+                                <a href="#" className='item-link'><i className="fa-brands fa-square-github"></i></a>
+                                <a href="#" className='item-link'><i className="fa-solid fa-envelope"></i></a>
+                            </div>
+                        </div>
+
+                        <div className="presentation">
+                            <p><span>Desenvolvedora Web Júnior</span>, formada em <span>Sistemas para Internet</span> pela <span>Universidade Unicesumar</span> em 2022. Iniciei minha carreira na área de <span>Testes e Qualidade</span>, definindo regras de negócio, treinamentos de ferramentas desenvolvidas, testes manuais E2E e distribuição para a nossa rede de franqueados. </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
