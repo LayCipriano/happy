55ab4f0ec7e678d4b0a800b75f34ec14552364b8
Lay Cipriano <webdevlay@gmail.com>
2024-12-18 02:00:38 -0300

    melhorando a UX da exibição dos projetos

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,21 +1,38 @@
 import {useEffect, useState} from "react"
 import getProjects from "../services/ProjectService"; //api service
 import Projects from "./Projects"; //project list
+import SyncLoader from "react-spinners/SyncLoader"
 
 export default function Portfolio() {
 
   const [projetos, setProjetos] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    getProjects()
-    .then(data => setProjetos(data))
-    .catch(error => console.log("Erro ao buscar a lista de projetos!", error))
-  }, [])
-  
-  return (
-    <div className="boxes">
-      {projetos.map(projeto => <Projects data={projeto}/>)}
-    </div >
-  )
+    useEffect(() => {
+        setLoading(true) // loading true enquanto os dados são buscados
+        getProjects()
+        .then((data) => {
+          setProjetos(data)
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar a lista de projetos!", error);
+        })
+        .finally(() => {
+          setLoading(false) // loading false, os dados foram carregados
+        })
+    }, [])
+
+    return (
+      <>
+      {loading ? (
+        <div className="loading"><SyncLoader color="#ffffff"/></div>
+      ) : (
+
+      <div className="boxes">
+          {projetos.map(projeto => <Projects data={projeto}/>)}
+      </div>
+      )}
+      </>
+    )
+
 }
-
